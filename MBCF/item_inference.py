46b4a4193a7b28985a2fd2942b2a7889e5a0eb5c
KIM EUNWOO <kimeunwoo0125@gmail.com>
2024-02-01 06:56:53 +0000

    [MODEL] item based CF 추가
    기존 Inference.py를 item_inference.py로 수정
    새로운 Item_inference.py 추가
    
    #2

--- MBCF/item_inference.py
+++ MBCF/item_inference.py
@@ -25,40 +25,38 @@ user_indices = df["user"].values
 item_indices = df["item"].values
 train_matrix[user_indices, item_indices] = 1.0
 
-# calculate cosine similarity
-
 
 try:
-    result_matrix = np.load("./result_matrix.npy")
+    result_matrix = np.load("./item_result_matrix.npy")
     print("npy loaded")
 except:
+    # calculate cosine similarity
     print("similarity")
-    user_sim = np.ones((n_users, n_users), dtype=float)
-    item_sim = np.ones((n_items, n_items), dtype=float)  # not used in this version.
+    item_sim = np.ones((n_items, n_items), dtype=float)
     """
-    for i in tqdm(range(n_users)):
-        user_sim[i, i] = 1.0
-        for j in range(i+1, n_users):
-            cosine_sim = np.inner(train_matrix[i],train_matrix[j]) / (np.sum(train_matrix[i]) * np.sum(train_matrix[j]))
-            user_sim[i, j] = cosine_sim
-            user_sim[j, i] = cosine_sim
+    for i in tqdm(range(n_items)):
+        item_sim[i, i] = 1.0
+        for j in range(i+1, n_items):
+            cosine_sim = np.inner(train_matrix[:,i],train_matrix[:,j]) / (np.sum(train_matrix[:,i]) * np.sum(train_matrix[:,j]))
+            item_sim[i, j] = cosine_sim
+            item_sim[j, i] = cosine_sim
     """
     train_matrix_normalized = train_matrix / np.linalg.norm(
-        train_matrix, axis=1, keepdims=True
+        train_matrix, axis=0, keepdims=True
     )
-    user_sim = train_matrix_normalized @ train_matrix_normalized.T
-    user_sim[np.isnan(user_sim)] = 0.0
-    np.fill_diagonal(user_sim, 1.0)
+    item_sim = train_matrix_normalized.T @ train_matrix_normalized
+    item_sim[np.isnan(item_sim)] = 0.0
+    np.fill_diagonal(item_sim, 1.0)
 
     # perform collaborative filetering
     print("CF")
     """result_matrix = np.zeros((n_users, n_items), dtype=float)
-    for i in tqdm(range(n_users)):
-        for j in range(n_users):
-            result_matrix += train_matrix[j] * user_sim[i, j]
-    result_matrix /= np.sum(user_sim, axis=1)"""
-    result_matrix = user_sim @ train_matrix
-    np.save("./result_matrix.npy", result_matrix)
+    for i in tqdm(range(n_items)):
+        for j in range(n_items):
+            result_matrix[:,i] += train_matrix[:,j] * item_sim[i, j]
+    result_matrix /= np.sum(item_sim, axis=0)"""
+    result_matrix = train_matrix @ item_sim
+    np.save("./item_result_matrix.npy", result_matrix)
 print("cf_matrix shape:", result_matrix.shape)
 
 
@@ -81,4 +79,4 @@ for i in range(n_users):
     submission_df = pd.concat(
         (submission_df, pd.DataFrame({"user": [user_id] * top_n, "item": top_items}))
     )
-submission_df.to_csv("submission.csv", index=False)
+submission_df.to_csv("item_submission.csv", index=False)
