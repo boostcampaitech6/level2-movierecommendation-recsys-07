4b648b1b3366055ff25767dfef95b9aecbb8e8e4
JunHyuck <wnsgur1615@naver.com>
2024-02-01 05:27:00 +0000

    [FIX] Doc string 추가, 사용성 개선
    #6

--- code/Word2Vec/inertia.py
+++ code/Word2Vec/inertia.py
@@ -5,10 +5,18 @@ import matplotlib.pyplot as plt
 
 from tqdm import tqdm
 
-def main():
-    df_name = "Word2Vec_not_shuffle_emb_df"
-    df = pd.read_csv(f'{df_name}.csv')
-    ks = range(1,20)
+import warnings
+
+warnings.filterwarnings(action="ignore")
+
+
+def inertia(df_name: str) -> None:
+    """
+    KMeans clustering을 할 때 적절한 k값을 찾기 위함.
+    결과 그래프에서 급격히 꺾이며 완만해지는 구간이 적절한 k.
+    """
+    df = pd.read_csv(f"{df_name}.csv")
+    ks = range(1, 30)
     inertias = []
 
     for k in tqdm(ks):
@@ -17,13 +25,14 @@ def main():
         inertias.append(model.inertia_)
 
     # Plot ks vs inertias
-    plt.figure(figsize=(4, 4))
+    plt.figure(figsize=(12, 12))
 
-    plt.plot(ks, inertias, '-o')
-    plt.xlabel('number of clusters, k')
-    plt.ylabel('inertia')
+    plt.plot(ks, inertias, "-o")
+    plt.xlabel("number of clusters, k")
+    plt.ylabel("inertia")
     plt.xticks(ks)
-    plt.savefig(f'{df_name}_Intertias.png')
+    plt.savefig(f"{df_name}_Intertias.png")
 
-if __name__ == '__main__':
-    main()
+
+if __name__ == "__main__":
+    inertia("Word2Vec_not_shuffle_emb_df")
