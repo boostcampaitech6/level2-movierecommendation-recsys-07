68a33fef426c2d49bb5dcf6112161ad495692f90
JunHyuck <wnsgur1615@naver.com>
2024-02-03 00:55:09 +0000

    [DATA] MF 임베딩 시각화 구현
    #14

--- code/MF/model.py
+++ code/MF/model.py
@@ -5,15 +5,22 @@ import torch.nn as nn
 class MF(nn.Module):
     def __init__(self, args):
         super().__init__()
-        self.user_dims = args.n_users
-        self.item_dims = args.n_items
+        try:  # visualize를 위해 embedding을 꺼낼 때 args.n_users가 없음.
+            self.user_dims = args.n_users
+            self.item_dims = args.n_items
+        except:
+            self.user_dims = 31360
+            self.item_dims = 6807
         # hidden_dim을 embeddding dim으로 사용
         self.user_embedding = nn.Embedding(self.user_dims, args.hidden_dim)
         self.item_embedding = nn.Embedding(self.item_dims, args.hidden_dim)
         torch.nn.init.xavier_normal_(self.user_embedding.weight.data)
         torch.nn.init.xavier_normal_(self.item_embedding.weight.data)
         # 전체 정답률 평균
-        self.mu = args.n_rows / (args.n_rows + args.n_users * args.n_neg)
+        try:  # visualizae를 위해 embedding을 꺼낼 때 args.n_rows가 없음.
+            self.mu = args.n_rows / (args.n_rows + args.n_users * args.n_neg)
+        except:
+            pass
         self.b_u = nn.Parameter(torch.zeros(self.user_dims))
         self.b_i = nn.Parameter(torch.zeros(self.item_dims))
 
