9210d0c4426596d8fa951f04384e9879d493a178
Jinhos <namjino1018@gmail.com>
2024-02-04 14:51:13 +0000

    [FEAT] preprocess 과정 추가
    #22

--- TF-IDF/TF-IDF.py
+++ TF-IDF/TF-IDF.py
@@ -4,6 +4,7 @@ warnings.filterwarnings("ignore")
 import pandas as pd
 import numpy as np
 import os
+import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.metrics.pairwise import cosine_similarity
@@ -20,6 +21,80 @@ director_data = pd.read_csv("/opt/ml/input/data/train/directors.tsv", sep="\t")
 
 test_df = pd.read_csv("/opt/ml/input/data/eval/sample_submission.csv")  # 예제 데이터
 print("test shape: ", test_df.shape)
+
+# preprocess #####################################################################################
+
+# 1900년부터 2099년까지의 년도 추출 및 새로운 컬럼 생성
+title_data["year"] = title_data["title"].str.extract(r"\((19[0-9]{2}|20[0-9]{2})\)")
+
+# 예외 데이터에 대한 처리
+# "Fawlty Towers (1975-1979)"의 경우 1975 추출, "Big Bang Theory, The (2007-)"의 경우 2007 추출
+title_data["year"] = title_data.apply(
+    lambda row: (
+        re.search(r"\((\d{4})", row["title"]).group(1)
+        if pd.isna(row["year"])
+        else row["year"]
+    ),
+    axis=1,
+)
+
+# 'title' 컬럼에서 괄호와 괄호 안의 내용 삭제
+title_data["title"] = (
+    title_data["title"].str.replace(r"\(.*?\)", "", regex=True).str.strip()
+)
+
+# 관사 및 전치사를 포함하는 정규 표현식 패턴
+pattern = r"\b(?:a|an|the|for|in|of|on|to|with)\b"
+
+# 관사와 전치사 제거
+title_data["title"] = (
+    title_data["title"].str.replace(pattern, "", flags=re.IGNORECASE).str.strip()
+)
+
+# 'year' 컬럼을 문자열로 변환하고 'title' 컬럼과 결합
+title_data["title"] = title_data["title"] + " " + title_data["year"].astype(str)
+title_data = title_data.drop("year", axis=1)
+
+# director #######################################################################################
+
+title_data = pd.merge(title_data, director_data, on="item", how="outer")
+# 'director' 컬럼에서 NaN 값을 빈 문자열로 변환하고 결합
+title_data = (
+    title_data.groupby(["item", "title"])["director"]
+    .agg(lambda x: " ".join(x.fillna("")))
+    .reset_index()
+)
+# 'director' 컬럼을 'title' 컬럼과 결합
+title_data["title"] = title_data["title"] + " " + title_data["director"]
+title_data = title_data.drop("director", axis=1)
+
+# writer #########################################################################################
+
+title_data = pd.merge(title_data, writer_data, on="item", how="outer")
+# 'writer' 컬럼에서 NaN 값을 빈 문자열로 변환하고 결합
+title_data = (
+    title_data.groupby(["item", "title"])["writer"]
+    .agg(lambda x: " ".join(x.fillna("")))
+    .reset_index()
+)
+# 'writer' 컬럼을 'title' 컬럼과 결합
+title_data["title"] = title_data["title"] + " " + title_data["writer"]
+title_data = title_data.drop("writer", axis=1)
+
+# genre ##########################################################################################
+
+title_data = pd.merge(title_data, genre_data, on="item", how="outer")
+# 'genre' 컬럼에서 NaN 값을 빈 문자열로 변환하고 결합
+title_data = (
+    title_data.groupby(["item", "title"])["genre"]
+    .agg(lambda x: " ".join(x.fillna("")))
+    .reset_index()
+)
+# 'genre' 컬럼을 'title' 컬럼과 결합
+title_data["title"] = title_data["title"] + " " + title_data["genre"]
+title_data = title_data.drop("genre", axis=1)
+
+##################################################################################################
 
 
 movie_train_df = pd.merge(train_df, title_data, on="item")
