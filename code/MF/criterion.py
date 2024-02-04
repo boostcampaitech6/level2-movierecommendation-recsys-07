93c236cb9d8a39a8c7c87f6b87f95767a7553fd0
JunHyuck <wnsgur1615@naver.com>
2024-02-04 05:16:29 +0000

    [FEAT] BPR loss 구현
    #14

--- code/MF/criterion.py
+++ code/MF/criterion.py
@@ -8,6 +8,12 @@ def get_criterion(pred: torch.Tensor, target: torch.Tensor, args):
         loss = torch.mean(loss)
     elif args.loss_function.lower() == "roc_star":
         loss = roc_star_paper(pred, target, args)
+    elif args.loss_function.lower() == "bpr":
+        loss = BPR(pred, target, args)
+    else:
+        raise NotImplementedError(
+            f"loss function {args.loss_function} is not implemented"
+        )
 
     return loss
 
@@ -42,3 +48,35 @@ def roc_star_paper(y_pred, _y_true, args):
     loss = torch.sum(diff * diff)
     loss = loss / (ln_pos + ln_neg)
     return loss + 1e-8
+
+
+def BPR(pred, target, args):
+    y_true = target > 0.5
+
+    # if batch is either all true or false return small random stub value.
+    if torch.sum(y_true) == 0 or torch.sum(y_true) == target.shape[0]:
+        return torch.tensor(target.shape[0] * 1e-8, requires_grad=True)
+
+    pos = pred[y_true]
+    neg = pred[~y_true]
+
+    ln_pos = pos.shape[0]
+    ln_neg = neg.shape[0]
+
+    max_pos = 1000  # Max number of positive training samples
+    max_neg = 1000  # Max number of positive training samples
+    pos = pos[torch.rand_like(pos) < max_pos / ln_pos]
+    neg = neg[torch.rand_like(neg) < max_neg / ln_neg]
+
+    ln_pos = pos.shape[0]
+    ln_neg = neg.shape[0]
+
+    pos_expand = pos.view(-1, 1).expand(-1, ln_neg).reshape(-1)
+    neg_expand = neg.repeat(ln_pos)
+
+    diff = -(pos_expand - neg_expand)
+    diff = diff[diff > 0]
+
+    loss = -torch.sum(torch.log(torch.sigmoid(diff))) / (ln_pos + ln_neg)
+
+    return loss
