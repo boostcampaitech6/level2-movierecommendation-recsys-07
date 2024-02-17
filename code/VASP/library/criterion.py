2d8cf4404c779761151f293f4bfb0a819687a10f
KIM EUWNOO <kimeunwoo0125@gmail.com>
2024-02-17 15:09:53 +0000

    [INIT] convert nease file to train.py and other files
    #37

---
+++ code/VASP/library/criterion.py
@@ -0,0 +1,96 @@
+import torch
+
+
+def get_criterion(pred: torch.Tensor, target: torch.Tensor, args):
+    """if args.loss_function.name.lower() == "bce":
+        loss = torch.nn.BCEWithLogitsLoss(reduction="none")
+        loss = loss(pred, target)
+        loss = torch.mean(loss)
+    elif args.loss_function.name.lower() == "roc_star":
+        loss = roc_star_paper(pred, target, args)
+    elif args.loss_function.name.lower() == "bpr":
+        loss = BPR(pred, target, args)"""
+    try:
+        loss_function_name = args.loss_function.name.lower()
+        loss_function = {"original_ease": original_ease}.get(loss_function_name)
+        loss = loss_function(pred, target, args)
+    except:
+        raise NotImplementedError(
+            f"loss function {args.loss_function} is not implemented"
+        )
+    return loss
+
+
+def roc_star_paper(y_pred: torch.Tensor, _y_true: torch.Tensor, args):
+    y_true = _y_true >= 0.5
+
+    # if batch is either all true or false return small random stub value.
+    if torch.sum(y_true) == 0 or torch.sum(y_true) == y_true.shape[0]:
+        return torch.tensor(y_pred.shape[0] * 1e-8, requires_grad=True)
+
+    pos = y_pred[y_true]
+    neg = y_pred[~y_true]
+
+    ln_pos = pos.shape[0]
+    ln_neg = neg.shape[0]
+
+    max_pos = 1000  # Max number of positive training samples
+    max_neg = 1000  # Max number of positive training samples
+    pos = pos[torch.rand_like(pos) < max_pos / ln_pos]
+    neg = neg[torch.rand_like(neg) < max_neg / ln_neg]
+
+    ln_pos = pos.shape[0]
+    ln_neg = neg.shape[0]
+
+    pos_expand = pos.view(-1, 1).expand(-1, ln_neg).reshape(-1)
+    neg_expand = neg.repeat(ln_pos)
+
+    diff = -(pos_expand - neg_expand - args.loss_function.gamma)
+    diff = diff[diff > 0]
+
+    loss = torch.sum(diff * diff)
+    loss = loss / (ln_pos + ln_neg)
+    return loss + 1e-8
+
+
+def BPR(pred: torch.Tensor, target: torch.Tensor, args):
+    """
+    Bayesian Personalized Ranking loss
+    batch가 유저 단위로 구성되지 않기에 정확한 BRP은 아님."""
+    y_true = target > 0.5
+
+    # if batch is either all true or false return small random stub value.
+    if torch.sum(y_true) == 0 or torch.sum(y_true) == target.shape[0]:
+        return torch.tensor(target.shape[0] * 1e-8, requires_grad=True)
+
+    pos = pred[y_true]
+    neg = pred[~y_true]
+
+    ln_pos = pos.shape[0]
+    ln_neg = neg.shape[0]
+
+    max_pos = 1000  # Max number of positive training samples
+    max_neg = 1000  # Max number of positive training samples
+    pos = pos[torch.rand_like(pos) < max_pos / ln_pos]
+    neg = neg[torch.rand_like(neg) < max_neg / ln_neg]
+
+    ln_pos = pos.shape[0]
+    ln_neg = neg.shape[0]
+
+    pos_expand = pos.view(-1, 1).expand(-1, ln_neg).reshape(-1)
+    neg_expand = neg.repeat(ln_pos)
+
+    diff = -(pos_expand - neg_expand)
+    diff = diff[diff > 0]
+
+    loss = -torch.sum(torch.log(torch.sigmoid(diff))) / (ln_pos + ln_neg)
+
+    return loss
+
+
+def original_ease(recon_x, x, args):
+    loss = x.sub(recon_x)
+    loss = loss**2
+    loss = loss.sum()
+    regularization = torch.norm(recon_x)
+    return loss + args.loss_function.lambd * regularization
---
+++ code/VASP/train.py
@@ -0,0 +1,64 @@
+import os
+from datetime import datetime
+
+import torch
+import wandb
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+from library.utils import get_logger, set_seeds, logging_conf
+from library.loader import DataLoader
+from library.trainer import get_model, run
+
+logger = get_logger(logging_conf)
+
+
+@hydra.main(version_base="1.3", config_path="config", config_name="default.yaml")
+def main(args: DictConfig):
+    # seed and device
+    OmegaConf.set_struct(args, False)
+    set_seeds(args.seed)
+    args.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    # wandb initialize
+    wandb.login()
+    wandb.init(
+        project="vasp_dev",  # + args.model.name,
+        config=OmegaConf.to_container(args, resolve=True, throw_on_missing=True),
+        entity="buzzer_beater",
+    )
+
+    # make and set directory to save a model
+    os.makedirs(args.model_dir, exist_ok=True)
+    args.model_dir = os.path.join(
+        args.model_dir,
+        args.model.name.lower(),
+        datetime.utcfromtimestamp(wandb.run.start_time).strftime("%Y-%m-%d_%H:%M:%S")
+        + wandb.run.name,
+    )
+    os.makedirs(args.model_dir, exist_ok=True)
+
+    logger.info("Preparing data ...")
+    loader = DataLoader(args)
+
+    args.n_items = loader.load_n_items()
+    train_data = loader.load_data("train")
+    vad_data_tr, vad_data_te = loader.load_data("validation")
+    test_data_tr, test_data_te = loader.load_data("test")
+    data = [train_data, vad_data_tr, vad_data_te, test_data_tr, test_data_te]
+
+    args.N = train_data.shape[0]
+    args.idxlist = list(range(args.N))
+
+    logger.info("Building Model ...")
+    model = get_model(args).to(args.device)
+
+    logger.info("Start Training ...")
+    run(args, model, data)
+
+    logger.info("Saving configuration")
+    OmegaConf.save(config=args, f=os.path.join(args.model_dir, "default.yaml"))
+
+
+if __name__ == "__main__":
+    main()
